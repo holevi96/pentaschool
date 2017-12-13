6b83016dbe093b764571326edeb673ea4ba0e4f5
Horváth Levi <holevi96@gmail.com>
2017-12-13 16:32:38 +0100

    Adding variant - and test header

--- wp-content/themes/penta/templates/header.php
+++ wp-content/themes/penta/templates/header.php
@@ -1,12 +1,95 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
-</header>
+        <div class="nav-container">
+            <div>
+                <div class="bar bar--sm visible-xs">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-3 col-sm-2">
+                                <a href="index.html"> <img class="logo logo-dark" alt="logo" src="img/logo-dark.png"> <img class="logo logo-light" alt="logo" src="img/logo-light.png"> </a>
+                            </div>
+                            <div class="col-xs-9 col-sm-10 text-right">
+                                <a href="#" class="hamburger-toggle" data-toggle-class="#menu1;hidden-xs hidden-sm"> <i class="icon icon--sm stack-interface stack-menu"></i> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <nav id="menu1" class="bar bar-1 hidden-xs">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-1 col-sm-2 hidden-xs">
+                                <div class="bar__module">
+                                    <a href="index.html"> <img class="logo logo-dark" alt="logo" src="img/logo-dark.png"> <img class="logo logo-light" alt="logo" src="img/logo-light.png"> </a>
+                                </div>
+                            </div>
+                            <div class="col-md-11 col-sm-12 text-right text-left-xs text-left-sm">
+                                <div class="bar__module">
+                                    <ul class="menu-horizontal text-left">
+                                        <li> <a href="#">
+                                        Single Link
+                                    </a> </li>
+                                        <li class="dropdown"> <span class="dropdown__trigger">
+                                        Dropdown Slim
+                                    </span>
+                                            <div class="dropdown__container">
+                                                <div class="container">
+                                                    <div class="row">
+                                                        <div class="dropdown__content col-md-2">
+                                                            <ul class="menu-vertical">
+                                                                <li> <a href="#">Single Link</a> </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="dropdown"> <span class="dropdown__trigger">
+                                        Dropdown Wide
+                                    </span>
+                                            <div class="dropdown__container">
+                                                <div class="container">
+                                                    <div class="row">
+                                                        <div class="dropdown__content col-md-12">
+                                                            <div class="col-md-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-md-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-md-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="col-md-3">
+                                                                <h5>Menu Title</h5>
+                                                                <ul class="menu-vertical">
+                                                                    <li> <a href="#">Single Link</a> </li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="bar__module">
+                                    <a class="btn btn--sm type--uppercase" href="#customise-template"> <span class="btn__text">
+                                    Try Builder
+                                </span> </a>
+                                    <a class="btn btn--sm btn--primary type--uppercase" href="#purchase-template"> <span class="btn__text">
+                                    Buy Now
+                                </span> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
