c967e427c5280709d897e8801239a1d9c3cd34c2
Horváth Levi <holevi96@gmail.com>
2017-12-13 16:53:38 +0100

    template dir

--- wp-content/themes/penta/templates/head.php
+++ wp-content/themes/penta/templates/head.php
@@ -5,12 +5,12 @@
   <meta charset="utf-8">
         <title><?php wp_title( 'Pentaschool', true, 'right' ); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all">
-        <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/theme-charcoal.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/stack-interface.css" rel="stylesheet" type="text/css" media="all">
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/theme-charcoal.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/custom.css" rel="stylesheet" type="text/css" media="all" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
