1cdca078077e42df02c93548ed7098b25946e11a
Unknown <holevi96@gmail.com>
2018-01-28 17:09:01 +0100

    clcik

--- wp-content/themes/penta/js/single-tanfolyamok.js
+++ wp-content/themes/penta/js/single-tanfolyamok.js
@@ -1,12 +1,10 @@
 jQuery(document).ready(function(){
-    jQuery("#card").flip({
-        trigger: 'manual'
-    });
+
     jQuery('.tanfolyam-kiirasok-box ul li').eq(0).click();
 
     jQuery( "body" ).delegate( ".reszletes_idopontok", "click", function(e) {
         console.log('asd')
         e.preventDefault();
-        jQuery("#card").flip('toggle');
+        jQuery(".card").flip('toggle');
     });
 })
