4e743f9aae33a9a3be3e0e5a853cab380d2b6075
Unknown <holevi96@gmail.com>
2018-01-28 16:38:10 +0100

    f

--- wp-content/themes/penta/js/single-tanfolyamok.js
+++ wp-content/themes/penta/js/single-tanfolyamok.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function(){
     });
     jQuery('.tanfolyam-kiirasok-box ul li').eq(0).click();
     jQuery('.reszletes_idopontok').click(function(e){
+        console.log('asd')
         e.preventDefault();
         jQuery("#card").flip('toggle');
     })
