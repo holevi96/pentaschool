b0978c09edff98714bd15bb9eafe444ab2013da7
Unknown <holevi96@gmail.com>
2018-01-28 15:40:46 +0100

    fix

--- wp-content/themes/penta/templates/head.php
+++ wp-content/themes/penta/templates/head.php
@@ -11,7 +11,7 @@
         <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
         <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/flickity.css" rel="stylesheet" type="text/css" media="all" />
         <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/theme-charcoal.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/theme.css" rel="stylesheet" type="text/css" media="all" />
         <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/custom.css" rel="stylesheet" type="text/css" media="all" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700" rel="stylesheet">
   <?php wp_head(); ?>
