0f2e3a2817d23112260dda75ca53a166cd25de05
Unknown <holevi96@gmail.com>
2018-01-28 16:19:28 +0100

    flip fix

--- wp-content/themes/penta/js/single-tanfolyamok.js
+++ wp-content/themes/penta/js/single-tanfolyamok.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function(){
+    jQuery("#card").flip({
+        trigger: 'manual'
+    });
     jQuery('.tanfolyam-kiirasok-box ul li').eq(0).click();
     jQuery('.reszletes_idopontok').click(function(){
         jQuery("#card").flip('toggle');
