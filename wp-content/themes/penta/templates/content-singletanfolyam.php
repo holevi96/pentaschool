b58f5c4b7bc90fb3e33760b93ec47ea4a68d6c87
Unknown <holevi96@gmail.com>
2018-02-03 20:25:29 +0100

    new idea

--- wp-content/themes/penta/templates/content-singletanfolyam.php
+++ wp-content/themes/penta/templates/content-singletanfolyam.php
@@ -80,28 +80,38 @@
                           <hr>
                         </ul>
                       </div>
-                        <div class="col-md-4 col boxed boxed--border card" >
+                        <div class="col-md-4 col boxed boxed--border" >
 
                                 <div class="front  box-shadow bg--secondary feature" style="">
                                     <h4>Jelentkezz a tanfolyamra!</h4>
 
-                                    <p>
-                                        <span class="tanf-price"><?php echo get_field('akcios_ar', $kiiras->ID); ?>
-                                            Ft</span> <strike><?php the_field('listaar'); ?> Ft.</strike>
-                                    </p>
+
 
                                     <div class="idopontok boxed boxed--border" style=" margin-top: 10px;">
                                         <!--<i class="icon icon--sm icon-Stopwatch"></i> <span>7 alkalom:</span>-->
 
-                                        <h4>Napszak:</h4>
-                                        <p><?php echo get_field('napszak', $kiiras->ID); ?></p>
 
-                                        <h4>Időbeosztás:</h4>
+                                        <p>
+                                        <span class="tanf-price"><?php echo get_field('akcios_ar', $kiiras->ID); ?>
+                                            Ft</span> <strike><?php the_field('listaar'); ?> Ft.</strike>
+                                        </p>
+                                        <h4 style="color:#fff;" class="color--primary1 .type--uppercase">Egyéb lehetőségek: </h4>
+                                        <p>
+                                            <a class="btn btn--lg btn more-options-button" href="file:///C:/Users/DELL/Documents/themeforest-19337626-stack-multipurpose-html-with-page-builder/Stack%201.5.1/tanfolyam-oldal2.html#" >
+                                                <span class="btn__text">Érdekel egy <br/> későbbi időpontban!</span>
+                                            </a>
+                                        </p>
+                                        <p>
+                                            <a class="btn btn--lg more-options-button" href="file:///C:/Users/DELL/Documents/themeforest-19337626-stack-multipurpose-html-with-page-builder/Stack%201.5.1/tanfolyam-oldal2.html#" style="background: #fff;">
+                                                <span class="btn__text">Kérdésem van!</span>
+                                            </a>
+                                        </p>
+                                        <p>
+                                            <a class=" btn btn--lg more-options-button" href="file:///C:/Users/DELL/Documents/themeforest-19337626-stack-multipurpose-html-with-page-builder/Stack%201.5.1/tanfolyam-oldal2.html#" style="background: #fff;">
+                                                <span class="btn__text">Céges ajánlatot kérek!</span>
+                                            </a>
+                                        </p>
 
-                                        <p><?php echo get_field('ido_beosztas', $kiiras->ID); ?></p>
-                                        <a class="btn btn--sm btn--primary reszletes_idopontok">
-                                            <span class="btn__text" style="color:#fff">Időpontok részletesen</span>
-                                        </a>
                                     </div>
                                     <a class="btn btn--lg btn--primary"
                                        href="file:///C:/Users/DELL/Documents/themeforest-19337626-stack-multipurpose-html-with-page-builder/Stack%201.5.1/tanfolyam-oldal2.html#">
@@ -109,30 +119,23 @@
                                     </a>
                                 </div>
 
-                            <div class="back  box-shadow bg--secondary feature" style="">
-                                <a class="btn btn--sm btn--primary reszletes_idopontok">
-                                    <span class="btn__text" style="color:#fff">Bezárás</span>
-                                </a>
-                            </div>
+<!--                            <div class="back  box-shadow bg--secondary feature" style="">-->
+<!--                                <a class="btn btn--sm btn--primary reszletes_idopontok">-->
+<!--                                    <span class="btn__text" style="color:#fff">Bezárás</span>-->
+<!--                                </a>-->
+<!--                            </div>-->
                         </div>
                       <div class="col-md-4 col boxed boxed--border bg--primary">
                         <div class=" box-shadow">
-                          <h4 style="color:#fff;" class="color--primary1 .type--uppercase">Egyéb lehetőségek: </h4>
-                          <p>
-                            <a class="btn btn--lg btn more-options-button" href="file:///C:/Users/DELL/Documents/themeforest-19337626-stack-multipurpose-html-with-page-builder/Stack%201.5.1/tanfolyam-oldal2.html#" >
-                              <span class="btn__text">Érdekel egy <br/> későbbi időpontban!</span>
+                            <h4>Napszak:</h4>
+                            <p><?php echo get_field('napszak', $kiiras->ID); ?></p>
+                            <h4>Időbeosztás:</h4>
+
+                            <p><?php echo get_field('ido_beosztas', $kiiras->ID); ?></p>
+                            <a class="btn btn--sm btn--primary reszletes_idopontok">
+                                <span class="btn__text" style="color:#fff">Időpontok részletesen</span>
                             </a>
-                          </p>
-                          <p>
-                            <a class="btn btn--lg more-options-button" href="file:///C:/Users/DELL/Documents/themeforest-19337626-stack-multipurpose-html-with-page-builder/Stack%201.5.1/tanfolyam-oldal2.html#" style="background: #fff;">
-                              <span class="btn__text">Kérdésem van!</span>
-                            </a>
-                          </p>
-                          <p>
-                            <a class=" btn btn--lg more-options-button" href="file:///C:/Users/DELL/Documents/themeforest-19337626-stack-multipurpose-html-with-page-builder/Stack%201.5.1/tanfolyam-oldal2.html#" style="background: #fff;">
-                              <span class="btn__text">Céges ajánlatot kérek!</span>
-                            </a>
-                          </p>
+
 
                         </div>
                       </div>
