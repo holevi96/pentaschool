bd59276266816fe9cc6f9a34297342b46e979272
Unknown <holevi96@gmail.com>
2018-06-13 10:41:09 +0200

    added plugin

--- wp-content/themes/penta/templates/page-header.php
+++ wp-content/themes/penta/templates/page-header.php
@@ -1,5 +1,11 @@
 <?php use Roots\Sage\Titles; ?>
-
-<div class="page-header">
-  <h1><?= Titles\title(); ?></h1>
-</div>
+<section class="imagebg" data-overlay="4">
+    <div class="background-image-holder"> <img alt="background" src="<?php echo get_stylesheet_directory_uri(); ?>/img/cowork-7.jpg"> </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1><?= Titles\title(); ?></h1>
+            </div>
+        </div>
+    </div>
+</section>
